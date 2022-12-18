43b546d6926ba0bead1b6a4abc36ab109ab4da12
Mictronics <github@mictronics.de>
2022-12-18 17:50:09 +0100

    Fix webpack api error.

--- misc/webpack.browser.config.js
+++ misc/webpack.browser.config.js
@@ -1,37 +1,36 @@
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const banner = '/*! wmf.js (C) 2020-present SheetJS LLC -- https://sheetjs.com */';
+const banner =
+  '/*! wmf.js (C) 2020-present SheetJS LLC -- https://sheetjs.com */';
 
 module.exports = {
-	mode: 'development',
-	entry: './misc/entry.js',
-	devtool: 'source-map',
-	output: {
-		library: 'WMF',
-		libraryTarget: 'var',
-		filename: 'wmf.js'
-	},
-	node: { process: false },
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: ['source-map-loader'],
-				enforce: 'pre'
-			}
-		]
-	},
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				uglifyOptions: {
-					output: { beautify: false, preamble: banner }
-				}
-			})
-		]
-	},
-	plugins: [
-		new webpack.BannerPlugin({ banner, raw: true, entryOnly: true })
-	]
+  mode: 'development',
+  entry: './misc/entry.js',
+  devtool: 'source-map',
+  output: {
+    library: 'WMF',
+    libraryTarget: 'var',
+    filename: 'wmf.js'
+  },
+  node: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre'
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: { beautify: false, preamble: banner }
+        }
+      })
+    ]
+  },
+  plugins: [new webpack.BannerPlugin({ banner, raw: true, entryOnly: true })]
 };
